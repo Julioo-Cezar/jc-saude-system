9c74bc55351f6eb2057791806bc76180c52ad96e
Julioo-Cezar <JCnovaes.107@gmail.com>
2026-07-06 08:59:44 -0300

    Melhora layout e animação da página inicial

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* ====JC Saúde System JavaScript Principal=== */
+// Seleciona a barra de navegação
+const navbar=document.querySelector(".navbar");
+const header=document.querySelector(".header");
+
+// Escuta a rolagem da página
+window.addEventListener("scroll",function(){
+    if(window.scrollY>50){
+        navbar.classList.add("rolando");
+        navbar.classList.add("subir");
+        header.classList.add("esconder");
+    }else{
+        navbar.classList.remove("rolando");
+        navbar.classList.remove("subir");
+        header.classList.remove("esconder");
+    }
+});
+
+// Testes
+console.log("JavaScript funcionando!");
