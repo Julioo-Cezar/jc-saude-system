449281508d2c114198379b8bfcd72ef86c9f706e
Julioo-Cezar <JCnovaes.107@gmail.com>
2026-07-13 11:05:00 -0300

    adiciona contadores animados

--- js/script.js
+++ js/script.js
@@ -22,3 +22,41 @@ console.log("JavaScript funcionando!");
 // Ano automático do rodapé
 const ano= document.getElementById("ano");
 ano.textContent= new Date().getFullYear();
+
+// Contador animado
+const contadores= 
+document.querySelectorAll(".contador");
+const iniciarcontador=()=>{
+    contadores.forEach((contador)=>{
+        const alvo=
+        Number(contador.dataset.target);
+        let numero= 0;
+        const atualizar=()=> {
+            const incremento= Math.ceil(alvo/300);
+            numero+= incremento;
+            if(numero<alvo) {
+                contador.textContent=
+                numero.toLocaleString("pt-BR");
+
+                requestAnimationFrame(atualizar);
+            } else {
+                contador.textContent=
+                alvo.toLocaleString("pt-BR");
+               }
+            };
+            atualizar();
+          });
+        };
+        const secaoSobre=
+        document.querySelector("#sobre");
+        const observer= new
+        IntersectionObserver((entries)=> {
+if (entries[0].isIntersecting) {
+    iniciarcontador();
+    observer.disconnect();
+          }
+
+        },{
+            threshold:0.5
+    });
+observer.observe(secaoSobre);
